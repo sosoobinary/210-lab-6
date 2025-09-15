968402e1ff2f724e057dd01e350ec64365b84766
hackster07 <beylava101@gmail.com>
2025-09-14 17:40:47 -0700

    40 min

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -5,13 +5,29 @@ using namespace std;
 
 const int SIZE = 5;
 
+void enterArrayData(double* a);
+void outputArrayData(double* a);
+double sumArray(double* a);
+
+int main() {
+    double* fullArray;
+
+    enterArrayData(fullArray);
+    outputArrayData(fullArray);
+    sumArray(fullArray);
+}
+
 void enterArrayData(double* a) {
     a = new double[SIZE];
     double numInput;
     cout << "Data entry for the array:" << endl;
     for(int i = 0; i < SIZE; i++) {
         cout << "    > Element #" << i << ": ";
-        cin >> numInput;
+        while(!(cin >> numInput)){
+            cout << "Please enter a double for Element #" << i << ": ";
+            cin.clear();
+            cin.ignore(123, '\n');
+        }
         a[i] = numInput;
     }
     cout << "\nData entry complete." << endl;
@@ -22,4 +38,13 @@ void outputArrayData(double* a) {
     for(int i = 0; i < SIZE; i++) {
         cout << a[i] << " ";
     }
+    cout << endl;
+}
+
+double sumArray(double* a) {
+    double sum = 0;
+    for(int i = 0; i < SIZE; i++) {
+        sum += a[i];
+    }
+    return sum;
 }
