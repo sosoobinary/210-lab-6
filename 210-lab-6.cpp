bcc377a7d353d28d238f357667f1172d8ea3eac3
hackster07 <beylava101@gmail.com>
2025-09-14 17:21:39 -0700

    20 min

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,2 +1,16 @@
+// COMSC-210 | Lab 6 | Quang Ngo
+// IDE used: Visual Studio Code
+#include <iostream>
 using namespace std;
 
+const int SIZE = 5;
+
+void enterArrayData(double* a) {
+    a = new double[SIZE];
+    double numInput;
+    cout << "Data entry for the array:" << endl;
+    for(int i = 0; i < SIZE; i++) {
+        cout << "    > Element #" << i << ": ";
+        cin >> numInput;
+    }
+}
